0aef01b04cb1db16f9061fd7baff53b6c2ccc6f5
dymoc <dymoc@mail.ru>
2020-04-29 11:46:24 +0300

    Update pas_generator.php
    доработанн массив для выдора

--- Block_4/Site/pas_generator.php
+++ Block_4/Site/pas_generator.php
@@ -8,13 +8,13 @@
     <script type="text/javascript">
     var arreyNumbers = [];
     var numbers = [];
-    var arrey = ["a","b","c","d","e","f","g","h","t","m","l"]
+    var arrey = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
     
 
 
     //Рандомное число
     function getRandomNumber(askNumber) {
-        return Math.round(Math.random() * askNumber);
+        return Math.round(Math.random() * 62);
     }
 
     function readInt() {
