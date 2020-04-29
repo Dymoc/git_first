f9fda889544acb6b621e7734747c126fea0527b5
dymoc <dymoc@mail.ru>
2020-04-29 05:57:16 +0300

    Поправил табуляцию

--- Block_4/Site/index.php
+++ Block_4/Site/index.php
@@ -1,37 +1,42 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<title>Личный сайт студента GeekBrains</title>
-	<link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Личный сайт студента GeekBrains</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-<div class="content">
-	<?php
+    <div class="content">
+        <?php
     include "menu.php";
     ?>
-	<h1>Личный сайт студента GeekBrains</h1>
+        <h1>Личный сайт студента GeekBrains</h1>
 
-	<div class="center">
-	<img src="img/photo.png">
-		<div class="box_text">
-			<p><b>Добрый день</b>. Меня зовут <i>Духновский Александр</i>. Я совсем недавно начал программировать, однако уже написал свой первый сайт.</p>
+        <div class="center">
+            <img src="img/photo.png">
+            <div class="box_text">
+                <p><b>Добрый день</b>. Меня зовут <i>Духновский Александр</i>. Я совсем недавно начал программировать, однако уже написал свой первый сайт.</p>
 
-			<p>В этом мне помог IT-портал <a href="https://geekbrains.ru">GeekBrains</a></p>
+                <p>В этом мне помог IT-портал <a href="https://geekbrains.ru">GeekBrains</a></p>
 
-			<p>На этом сайте вы сможете сыграть в несколько игр, которые я написал: <br>
-			<a href="index.html">Главная</a>,
-			<a href="puzzle.html">Загадки</a>,
-			<a href="guess.html">Угадайка</a>
-			</p>
-		</div>
-	</div>
-</div>
-<div class="footer">
-	Copyright &copy; <?php echo date("Y")?> Aleksandr Duhnovskiy
-<div>
+                <p>На этом сайте вы сможете сыграть в несколько игр, которые я написал: <br>
+                    <a href="index.html">Главная</a>,
+                    <a href="puzzle.html">Загадки</a>,
+                    <a href="guess.html">Угадайка</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="footer">
+        Copyright &copy; <?php echo date("Y")?> Aleksandr Duhnovskiy
+    </div>div>
 
 
 </body>
+
 </html>
