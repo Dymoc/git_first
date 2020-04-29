121b0b81d77aa26315143613f4d1e9b980ad83cb
dymoc <dymoc@mail.ru>
2020-04-29 05:57:49 +0300

    табуляция

--- Block_4/Site/menu.php
+++ Block_4/Site/menu.php
@@ -1,6 +1,6 @@
 <div class="header">
-		<a href="index.php">Главная</a>
-		<a href="puzzle.php">Загадки</a>
-		<a href="guess.php">Угадайка</a>
-		<a href="ugad_2.php">Угадайка</a>
-	</div>
+    <a href="index.php">Главная</a>
+    <a href="puzzle.php">Загадки</a>
+    <a href="guess.php">Угадайка</a>
+    <a href="ugad_2.php">Угадайка</a>
+</div>
